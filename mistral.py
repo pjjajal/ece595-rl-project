cdb97c25493db0cb053546cbaba850a4457ab65b
pjjajal <jajalpurvish@gmail.com>
2023-11-05 20:56:45 -0500

    Lots of progress.

--- mistral.py
+++ mistral.py
@@ -1,13 +1,142 @@
-# Use a pipeline as a high-level helper
+import torch
+from awq import AutoAWQForCausalLM
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-AWQ")
-model = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-AWQ")
+from trl import AutoModelForCausalLMWithValueHead
 
 
-# pipe = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1", device="mps")
+class Agent:
+    def __init__(self) -> None:
+        # AWQ models are 4-bit quantized and really bloody fast.
+        model_name_or_path = "TheBloke/Mistral-7B-Instruct-v0.1-AWQ"
+        # model_name_or_path = "TheBloke/Mistral-7B-v0.1-AWQ"
+        # model_name_or_path = "mistralai/Mistral-7B-Instruct-v0.1"
 
-# while True:
-#     cmd = input("> ")
-#     out = pipe(cmd)
-#     print(out)
+        self.model = AutoAWQForCausalLM.from_quantized(
+            model_name_or_path,
+            fuse_layers=True,
+            trust_remote_code=False,
+            safetensors=True,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path, trust_remote_code=False
+        )
+        self.tokenizer.chat_template = "{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token + ' ' }}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}"
+
+        # model = AutoModelForCausalLM.from_pretrained(
+        #     "mistralai/Mistral-7B-Instruct-v0.1",
+        #     use_flash_attention_2=True,
+        #     device_map="auto",
+        #     load_in_8bit=True
+        # )
+        # tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+
+        self.chat = [
+            {
+                "role": "user",
+                "content": "You are playing TextWorld. To play TextWorld I will describe the evironment to you and you will issue commands. The TextWorld's text parser is limited, keep your answers to one command and 1-3 words. If you don't know what to do type 'help'.",
+            },
+            {
+                "role": "assistant",
+                "content": "I am playing TextWorld. I will issue commands based upon the environment that you describe that are 1-3 words long. Can you provide the objective?",
+            },
+            {
+                "role": "user",
+                "content": """
+                Your objective is to lift the stick of butter from the floor of the restroom.
+
+                You have the following commands available to you:
+                    look:                describe the current room
+                    goal:                print the goal of this game
+                    inventory:           print player's inventory
+                    go <dir>:            move the player north, east, south or west
+                    examine ...:         examine something more closely
+                    eat ...:             eat edible food
+                    open ...:            open a door or a container
+                    close ...:           close a door or a container
+                    drop ...:            drop an object on the floor
+                    take ...:            take an object that is on the floor
+                    put ... on ...:      place an object on a supporter
+                    take ... from ...:   take an object from a container or a supporter
+                    insert ... into ...: place an object into a container
+                    lock ... with ...:   lock a door or a container with a key
+
+                    
+                You will only return one of these commands.
+                """,
+            },
+            {
+                "role": "assistant",
+                "content": "My objective is to lift the stick of butter from the restroom. Describe my environment.",
+            },
+            {
+                "role": "user",
+                "content": """-= Restroom =-
+                You are in a restroom. It seems to be pretty typical here.
+                There is an unblocked exit to the north. You don't like doors? Why not try going west, that entranceway is unguarded.
+                There is a stick of butter on the floor.
+
+                What command will you take? Tell me in: <CMD>[YOUR COMMAND]</CMD>.""",
+            },
+        ]
+
+        # This is the original tokenization scheme.
+        # tokens = self.tokenizer.apply_chat_template(
+        #     self.chat, add_generation_prompt=True, return_tensors="pt"
+        # ).cuda()
+
+        # This applies the chat template but then does the assistant generation after a <CMD> tag. This should cause it to generate commands.
+        tokens = self.tokenizer.apply_chat_template(
+            self.chat, add_generation_prompt=True, tokenize=False
+        )
+        tokens = tokens + "<CMD>"
+        tokens = self.tokenizer(tokens, return_tensors="pt").input_ids.cuda()
+        input_length = tokens.shape[1]
+        # Generate output
+        generation_output = self.model.generate(
+            tokens,
+            do_sample=True,
+            temperature=0.8,
+            top_p=0.95,
+            top_k=40,
+            max_new_tokens=20,
+        )
+        decoded_outputs = self.tokenizer.batch_decode(
+            generation_output[:, input_length:], skip_special_tokens=True
+        )[0]
+        decoded_outputs = "<CMD>" + decoded_outputs
+        print(decoded_outputs)
+        self.chat.append({"role": "assistant", "content": decoded_outputs})
+
+        self.chat.append({"role": "user", "content": ""})
+
+    def act(self, obs):
+        # prompt = self.prompt_template(obs)
+        tokens = self.tokenizer.apply_chat_template(
+            self.chat, add_generation_prompt=True, return_tensors="pt"
+        ).cuda()
+        input_length = tokens.shape[1]
+        # Generate output
+        generation_output = self.model.generate(
+            tokens,
+            do_sample=True,
+            temperature=0.3,
+            top_p=0.95,
+            top_k=40,
+            max_new_tokens=20,
+        )
+
+        decoded_outputs = self.tokenizer.batch_decode(
+            generation_output[:, input_length:], skip_special_tokens=True
+        )[0]
+        print(decoded_output)
+
+
+if __name__ == "__main__":
+    agent = Agent()
+
+    # # _ = agent.act(x)
+    # # print(_)
+    # while True:
+    #     obs = input("")
+    #     action = agent.act(obs)
+    #     print(action)
--- textworld_test.py
+++ textworld_test.py
@@ -7,21 +7,21 @@ warnings.simplefilter("always")
 
 
 # Register a text-based game as a new Gym's environment.
-env_id = textworld.gym.register_game("tw_games/lev_1.z8", max_episode_steps=50)
-
+env_id = textworld.gym.register_game("games/thunt_1.z8", max_episode_steps=50)
+print(env_id)
 env = gym.make(env_id)  # Start the environment.
 
 obs, infos = env.reset()  # Start new episode.
 print(obs)
 # env.render()
 
-# score, moves, done = 0, 0, False
-# while not done:
-#     command = input("> ")
-#     obs, score, done, infos = env.step(command)
-#     print(obs, score, done, infos)
-#     env.render()
-#     moves += 1
+score, moves, done = 0, 0, False
+while not done:
+    command = input("> ")
+    obs, score, done, infos = env.step(command)
+    print(obs, score, done, infos)
+    # env.render()
+    moves += 1
 
 env.close()
 # print("moves: {}; score: {}".format(moves, score))
