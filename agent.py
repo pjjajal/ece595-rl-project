36bc0b9e95796df5725f1ce6ee38d7741b2366ff
pjjajal <jajalpurvish@gmail.com>
2023-11-27 16:31:48 -0500

    Updated agent.

--- agent.py
+++ agent.py
@@ -37,20 +37,76 @@ class Agent(ABC):
 
 
 class LlamaAgent(Agent):
-    def __init__(self) -> None:
+    def __init__(self, version) -> None:
         super().__init__()
+        model_name_or_path = f"TheBloke/Llama-2-{version}-Chat-AWQ"
+
+        self.model = AutoAWQForCausalLM.from_quantized(
+            model_name_or_path,
+            fuse_layers=True,
+            trust_remote_code=False,
+            safetensors=True,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path, trust_remote_code=False
+        )
 
     def _tokenize(self, obs):
-        return super()._tokenize(obs)
+        self.chat.append({"role": "user", "content": obs})
+        tokens = self.tokenizer.apply_chat_template(
+            self.chat, add_generation_prompt=True, tokenize=False
+        )
+        tokens = tokens + "<CMD>"
+        tokens = self.tokenizer(tokens, return_tensors="pt").input_ids.cuda()
+
+        return tokens
 
     def _detokenize(self, generation_output, input_length):
-        return super()._detokenize(generation_output, input_length)
+        decoded_outputs = self.tokenizer.batch_decode(
+            generation_output[:, input_length:], skip_special_tokens=True
+        )[0]
+        decoded_outputs = "<CMD>" + decoded_outputs
+        decoded_outputs = re.search(self.pattern, decoded_outputs)
+        decoded_outputs = decoded_outputs.group(1)
+        self.chat.append({"role": "assistant", "content": decoded_outputs})
+
+        return decoded_outputs
 
     def initial_action(self):
-        return super().initial_action()
+        tokens = self.tokenizer.apply_chat_template(
+            self.chat, add_generation_prompt=True, tokenize=False
+        )
+        print(tokens)
+        tokens = tokens + "<CMD>"
+        tokens = self.tokenizer(tokens, return_tensors="pt").input_ids.cuda()
+        input_length = tokens.shape[1]
+        # Generate output
+        generation_output = self.model.generate(
+            tokens,
+            do_sample=True,
+            temperature=0.5,
+            top_p=0.95,
+            top_k=40,
+            max_new_tokens=20,
+        )
+
+        decoded_outputs = self._detokenize(generation_output, input_length)
+        return decoded_outputs
 
     def act(self, obs):
-        return super().act(obs)
+        tokens = self._tokenize(obs)
+        input_length = tokens.shape[1]
+        # Generate output
+        generation_output = self.model.generate(
+            tokens,
+            do_sample=True,
+            temperature=0.6,
+            top_p=0.95,
+            top_k=40,
+            max_new_tokens=20,
+        )
+        decoded_outputs = self._detokenize(generation_output, input_length)
+        return decoded_outputs
 
 
 class MistralAgent:
@@ -83,44 +139,6 @@ class MistralAgent:
                 "role": "assistant",
                 "content": "I am playing TextWorld. I will issue commands based upon the environment that you describe that are 1-3 words long. Can you provide the objective?",
             },
-            # # TODO Programmatically generate this.
-            # {
-            #     "role": "user",
-            #     "content": """
-            #     Your objective is to lift the stick of butter from the floor of the restroom. You have the following commands available to you:
-            #         look:                describe the current room
-            #         goal:                print the goal of this game
-            #         inventory:           print player's inventory
-            #         go <dir>:            move the player north, east, south or west
-            #         examine ...:         examine something more closely
-            #         eat ...:             eat edible food
-            #         open ...:            open a door or a container
-            #         close ...:           close a door or a container
-            #         drop ...:            drop an object on the floor
-            #         take ...:            take an object that is on the floor
-            #         put ... on ...:      place an object on a supporter
-            #         take ... from ...:   take an object from a container or a supporter
-            #         insert ... into ...: place an object into a container
-            #         lock ... with ...:   lock a door or a container with a key
-
-                    
-            #     You will only return one of these commands.
-            #     """,
-            # },
-            # {
-            #     "role": "assistant",
-            #     "content": "My objective is to lift the stick of butter from the restroom. Describe my environment.",
-            # },
-            # # TODO Programmatic initial state
-            # {
-            #     "role": "user",
-            #     "content": """-= Restroom =-
-            #     You are in a restroom. It seems to be pretty typical here.
-            #     There is an unblocked exit to the north. You don't like doors? Why not try going west, that entranceway is unguarded.
-            #     There is a stick of butter on the floor.
-
-            #     What command will you take? Tell me in: <CMD>[YOUR COMMAND]</CMD>.""",
-            # },
         ]
 
     def _tokenize(self, obs):
@@ -185,11 +203,16 @@ class AgentFactory:
         if "mistral" in model_name:
             return MistralAgent()
         elif "llama" in model_name:
-            return LlamaAgent()
+            if "7b" in model_name or "7B" in model_name:
+                return LlamaAgent("7B")
+            elif "13b" in model_name or "13B" in model_name:
+                return LlamaAgent("13B")
+            elif "70b" in model_name or "70B" in model_name:
+                return LlamaAgent("70B")
 
 
 if __name__ == "__main__":
-    agent = AgentFactory.create('mistral')
+    agent = AgentFactory.create("mistral")
     while True:
         obs = input("")
         action = agent.act(obs)
